a4877c72132ee24d9cb62f1364af9ec62a46d85a
skyler <skyler@debian>
2025-12-28 10:48:48 +0200

    fix: updater installer script

--- install-updater.sh
+++ install-updater.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Installing dependency
-sudo apt install curl
 # Downloading the updater script
 curl -L -o discord-update.sh "https://raw.githubusercontent.com/SkylerHope/Debian-Discord-Updater/refs/heads/main/discord-update.sh"
+# Making the script executable
+chmod +x discord-update.sh
 # Moving the updater script to your PATH
 sudo mv discord-update.sh /usr/local/bin/discord-update
+
+echo "Done! You can run the discord-update command now!"
