0fb4c27eb405860c3d5747d53b6869452adfd306
skyler <skyler@debian>
2025-12-28 10:44:52 +0200

    feat: update installer done

--- install-updater.sh
+++ install-updater.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+# Installing dependency
+sudo apt install curl
+# Downloading the updater script
+curl -L -o discord-update.sh "https://raw.githubusercontent.com/SkylerHope/Debian-Discord-Updater/refs/heads/main/discord-update.sh"
+# Moving the updater script to your PATH
+sudo mv discord-update.sh /usr/local/bin/discord-update
